ae92f1bbebb1c12c1f0bec9dfa0a00326480d2a3
Myron <Myron_TO@outlook.com>
2025-12-03 15:39:36 +0800

    常规更新

--- nvim/after/lsp/basedpyright.lua
+++ nvim/after/lsp/basedpyright.lua
@@ -5,6 +5,11 @@ return {
         autoSearchPaths = true,
         diagnosticMode = "workspace",
         typeCheckingMode = "on",
+        configFilePath = vim.fn.getcwd() .. "/backend",
+        inlayHints = {
+          callArgumentNamesMatching = true,
+          functionReturnTypes = true
+        }
       },
     },
   },
