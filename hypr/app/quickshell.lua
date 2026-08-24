ba39b1054d86bfb7f0475553e562e5a2c22cc261
Myron <Myron_TO@outlook.com>
2026-08-24 11:41:58 +0800

    更新 yazi keymap 键位

--- hypr/app/quickshell.lua
+++ hypr/app/quickshell.lua
@@ -4,6 +4,7 @@ return {
     KeyBind(MainMod .. " + Space", Exec("noctalia msg panel-toggle launcher"))
     KeyBind(MainMod .. " + V", Exec("noctalia msg panel-toggle clipboard"))
     KeyBind(MainMod .. " + SHIFT + V", Exec("noctalia msg panel-toggle launcher /emo"))
+    KeyBind(MainMod .. " + C", Exec("noctalia msg panel-toggle launcher /calc"))
     KeyBind("XF86AudioRaiseVolume", Exec("noctalia msg volume-up"),
       { repeating = true, locked = true })
     KeyBind("XF86AudioLowerVolume", Exec("noctalia msg volume-down"),
